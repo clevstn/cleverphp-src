56d240a2f6eca3451e5d2036a4600d063db41c44
cleverstone <yang_hui_lei@163.com>
2020-05-04 23:30:09 +0800

    初始化框架核心库

--- src/App.php
+++ src/App.php
@@ -14,5 +14,55 @@ namespace clever;
 
 class App
 {
-    
+    // clever php version 1.0.0
+    const CLEVERPHP_VERSION = '1.0.0';
+
+    // application instance
+    public static $app;
+
+    /**
+     * App constructor.
+     */
+    public function __construct()
+    {
+        self::$app = $this;
+
+        // bootstrap
+        $this->bootstrap();
+    }
+
+    protected function bootstrap()
+    {
+        // register error and exception handler
+        $this->registerErrorAndExceptionHandler();
+        // register application configuration
+        $this->registerConfigure();
+        // register application debug record handler
+        $this->registerDebug();
+    }
+
+    protected function registerErrorAndExceptionHandler()
+    {
+
+    }
+
+    protected function registerConfigure()
+    {
+
+    }
+
+    protected function registerDebug()
+    {
+
+    }
+
+    /**
+     * Application gateway
+     *
+     * @author cleverstone
+     */
+    public function run()
+    {
+
+    }
 }
